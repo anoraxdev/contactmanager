bcc8aa78db0bb7cf19ac383491754841f12e1b14
Andreas Fault <andreas.fault@protonmail.com>
2019-03-04 19:05:35 -0600

    Updates

---
+++ precache-manifest.95d99630a28d90fceca297a006a8188e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "985b86c15761879d0144",
+    "url": "/contactmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "985b86c15761879d0144",
+    "url": "/contactmanager/static/js/main.985b86c1.chunk.js"
+  },
+  {
+    "revision": "7b70350a2f75277ec2e2",
+    "url": "/contactmanager/static/css/1.282de474.chunk.css"
+  },
+  {
+    "revision": "7b70350a2f75277ec2e2",
+    "url": "/contactmanager/static/js/1.7b70350a.chunk.js"
+  },
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "6da317de7517af355c04f6d66b4c8057",
+    "url": "/contactmanager/index.html"
+  }
+];
